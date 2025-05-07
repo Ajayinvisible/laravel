56232e886e4a3871749545d9969755374544210c
Ajay Rai <rai.invisibleweb@gmail.com>
2025-05-07 23:26:48 +0545

    message

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<x-frontend-layout.layouts>
+    <section class="my-20">
+        <div class="max-w-screen-xl mx-auto px-4">
+            <h1 class="text-red-500 text-3xl">Hello From About Page</h1>
+            <p class="text-gray-700">This is the about page of our application.</p>
+            <a href="{{ url('/') }}" class="text-blue-500 hover:underline">Go back to Home</a>
+        </div>
+    </section>
+</x-frontend-layout.layouts>
